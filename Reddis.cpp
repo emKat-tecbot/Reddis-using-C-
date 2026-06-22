1d1596ff347b7076401b5256fbdcc5f3c865df3a
EmKat-tecbot <emmamolinahdez@gmail.com>
2026-06-22 14:56:35 -0400

    Binded adress to os

--- Reddis.cpp
+++ Reddis.cpp
@@ -8,7 +8,15 @@
 int main(){
     // Obtain socket handle
     int fd = socket(AF_INET,SOCK_STREAM,0);
+    int val = 1; // keep SO_REUSEADDR activated so os dosent prevent you from using the port again after crash or restart
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+    //create network adress struct
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234); // port
+    addr.sin_addr.s_addr = htonl(0); //accept connection from anywhere (wildcard IP 0.0.0.0)
+    //give adress info to os (bind)
+    int rv = bind(fd,(const struct sockaddr*)&addr, sizeof(addr));
+    if(rv){die("bind()");} // exits if bind dosent succed(dosent return 0)
 
-    std::cout << "start";
-    return 0;
-};
+}
