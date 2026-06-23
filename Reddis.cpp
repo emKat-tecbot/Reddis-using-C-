9cb1425ce11ec43b61566bfd6bac72ef47127095
EmKat-tecbot <emmamolinahdez@gmail.com>
2026-06-22 23:38:25 -0400

    Demo server done: added connection from client side

--- Reddis.cpp
+++ Reddis.cpp
@@ -5,14 +5,19 @@
 #include <stdio.h> // error handling
 #include <cstring> // to zero out adress for structs
 
+static void die(const char* msg){
+    perror(msg);
+    exit(1);
+}
+
 static void do_something(int connfd){
     char rbuf[64] = {}; // buffer: stores clients message
-    ssize_t n = read(connfd,rbuf,sizeof(rbuf)-1) //read message from conection and put it in rbuf
+    ssize_t n = read(connfd,rbuf,sizeof(rbuf)-1); //read message from conection and put it in rbuf
     if(n < 0){ 
-        msg("read() error");
+        perror("read() error");
         return;
     }
-    cout << "client says: " << rbuf << endl;
+    std::cout << "client says: " << rbuf << endl;
     char wbuf[] = "world"; //message sent to the client
     write(connfd, wbuf, strlen(wbuf));
 }
@@ -27,7 +32,7 @@ void serverCon(){
     //create network adress struct
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(1234); // convert port to bid eddian so netwrok understands the port
+    addr.sin_port = ntohs(1234); // convert port to bid eddian so netwrok understands the port
     addr.sin_addr.s_addr = htonl(0); //accept connection from anywhere (wildcard IP 0.0.0.0)
 
     //give adress info to os (bind)
@@ -46,10 +51,43 @@ void serverCon(){
         //accept conections
         int connfd = accept(fd, (struct sockaddr*)&client_addr, &addrlen);
         if (connfd < 0){ 
-            continue; // dont crash server
+            continue; // dont crash server(
         }
         do_something(connfd); //interact with client (read and write)
         close(connfd); // close conection
     }
 
+}
+// conecting from the client side
+void clientCon(){
+    // Obtain socket handle
+    int fd = socket(AF_INET,SOCK_STREAM,0);
+    if(fd < 0){
+        die("socket()");
+    }
+    //create network addres
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234); // connect to the servers port
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); // test on same computer
+    
+    //connect
+    int rv = connect(fd,(const struct sockaddr*)&addr, sizeof(addr));
+    if(rv){
+        die("connect");
+    }
+    char msg[] = "hello"; // message client sends to server
+    write(fd,msg,strlen(msg));
+    char rbuf[64] = {}; //stores servers response
+    ssize_t n = read(fd,rbuf,sizeof(rbuf)-1);
+    if (n < 0){
+        die("read");
+    }
+    std::cout << "server says: " << rbuf << endl;
+    close(fd);
+}
+
+int main(){
+    clientCon();
+    return 0;
 }
